d4a843750dc194969baefede7249bed565b2320d
Rasmiranjan Sahoo <rasmiranjansahoo702@gmail.com>
2025-02-12 12:43:44 +0530

    completed all the 6 assignments of javascript day 1

--- day1.js
+++ day1.js
@@ -2,3 +2,24 @@
 var a ;
 a = 10;
 console.log(a);
+// Task 2 : declare a varibale using let , assign a string to it and log the value to the console.
+let b = "Hello";
+console.log(b);
+// Task 3 : Declare a variable using const , assign a boolean to it and log the value to the console.
+const c = true;
+console.log(c);
+// Task 4 : Declare a variable and do not assign any value to it. Log the value to the console.
+var d;
+console.log(d);
+// Task 5 : Declare a variable using let and assign it a initial value and reassign with it a new value.and log them 
+let e = 10;
+console.log(e);
+e = 20;
+console.log(e);
+// Task 6 : Declare a variable using const and reassign it with a new value and log the value to the console.observe the error.
+const f = 10;
+console.log(f);
+f = 20;
+console.log(f);
+
+
