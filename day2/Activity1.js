d4708312d2ef1e634790143a8590ebad34e68063
Rasmiranjan Sahoo <rasmiranjansahoo702@gmail.com>
2025-02-13 10:29:08 +0530

    Day 2 :- Activity 1 :- Arithmetic Operations

---
+++ day2/Activity1.js
@@ -0,0 +1,31 @@
+//Activity 1 :Arithmetic operations 
+
+//TASK1 :WRITE A PROGRAM TO ADD TWO NUMBERS AND LOG THE RESULT TO THE CONSOLE
+
+let a = 15;
+let b = 5;
+let sum = a + b ;
+console.log(`sum of a and b is ${sum}`);
+
+// Task 2 : WRITE A PROGRAM TO Subtract TWO NUMBERS AND LOG THE RESULT TO THE CONSOLE
+
+let subtract = a-b;
+console.log(`subtraction of two no is ${subtract}`);
+
+//task3 : WRITE A PROGRAM TO Multiply TWO NUMBERS AND LOG THE RESULT TO THE CONSOLE
+
+let multiply = a*b;
+console.log(`multiplication of and b ${multiply}`);
+
+
+// task 4 : WRITE A PROGRAM TO Devide TWO NUMBERS AND LOG THE RESULT TO THE CONSOLE
+
+let division = a/b;
+console.log(`dividion of a and b ${division}`);
+
+// task 5 :WRITE A PROGRAM TO get the remainder when  TWO NUMBERS get devided  AND LOG THE RESULT TO THE CONSOLE
+
+
+let reminder = a%b;
+console.log(`remainder when b devides a ${reminder}`);
+
