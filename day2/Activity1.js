5d00ef93a0df28ae8538f35931c35c858ffcff5e
Rasmiranjan Sahoo <rasmiranjansahoo702@gmail.com>
2025-02-13 10:36:36 +0530

    Day 2 :- Activity 2 :- Assignmenmt Operators

--- day2/Activity1.js
+++ day2/Activity1.js
@@ -15,17 +15,17 @@ console.log(`subtraction of two no is ${subtract}`);
 //task3 : WRITE A PROGRAM TO Multiply TWO NUMBERS AND LOG THE RESULT TO THE CONSOLE
 
 let multiply = a*b;
-console.log(`multiplication of and b ${multiply}`);
+console.log(`multiplication of a and b is ${multiply}`);
 
 
 // task 4 : WRITE A PROGRAM TO Devide TWO NUMBERS AND LOG THE RESULT TO THE CONSOLE
 
 let division = a/b;
-console.log(`dividion of a and b ${division}`);
+console.log(`dividion of a and b is ${division}`);
 
 // task 5 :WRITE A PROGRAM TO get the remainder when  TWO NUMBERS get devided  AND LOG THE RESULT TO THE CONSOLE
 
 
 let reminder = a%b;
-console.log(`remainder when b devides a ${reminder}`);
+console.log(`remainder when b devides a is ${reminder}`);
 
---
+++ day2/Activity2.js
@@ -0,0 +1,17 @@
+// Assignment Operators
+
+// Task 6 : use the += operator to add a nbumber to a variablke and log the result
+
+let a = 25;
+a += 10;
+console.log(a);
+
+
+// Task 7: Use the -= operator to subtract a number from a variable and log the result to the console.
+
+a -= 10;
+console.log(a);
+
+//a+=10 means a=a+10
+//similarly all opoerators act like this 
+//e.g a-=10 means a = a - 10
