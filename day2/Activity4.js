3f64d48e06095ba6447432f0ff240549ddcb3980
Rasmiranjan Sahoo <rasmiranjansahoo702@gmail.com>
2025-02-13 11:03:44 +0530

    Day2 :- Activity 4 :- Logical operators

---
+++ day2/Activity4.js
@@ -0,0 +1,30 @@
+// Activity 4: Logical Operators
+
+// Task 11: Write a program that uses the && operator to combine two conditions and log the result to the console.
+// it returns true when both the condition return true otherwise false 
+let a = 56;
+let b = 78;
+let c = 12;
+console.log(a<b && b>c);//true (because both the conditions are true)
+console.log(a<b && b<c);//false (because one of  the condition is false)
+
+
+
+// Task 12: Write a program that uses the || operator to combine two conditions and log the result to the console.
+// it returns false when both the condition return false otherwise true
+
+console.log(100<200 || 100>200);//true because one of them is true
+console.log(100>200 || 2>5);//false because none of them are true 
+
+
+// Task 13: Write a program that uses the ! operator to negate a condition and log the result to the console.
+//it converts true into false and viceversa
+
+console.log(!true);//false
+console.log(!5);//false
+console.log(!(2<5));//false
+
+
+
+
+
