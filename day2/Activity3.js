71e035c4a41ef8947340aed7bb7cbbbbf100fc3f
Rasmiranjan Sahoo <rasmiranjansahoo702@gmail.com>
2025-02-13 10:46:22 +0530

    Day 2 :- Activity 3 :- Comparison Operators

---
+++ day2/Activity3.js
@@ -0,0 +1,31 @@
+// Activity 3: Comparison Operators
+
+
+// Task 8: Write a program to compare two numbers using > and < and log the result to the console.
+
+let a = 25;
+let b = 35;
+let result = a > b;
+console.log(result);//false
+//assignment operators return true or false 
+console.log(a<b);//true
+
+
+// Task 9: Write a program to compare two numbers using>= and <= and log the result to the console.
+
+b = 55;
+let c = 55;
+console.log(b<=c);//true
+console.log(b>=c);// true
+
+// • Task 10: Write a program to compare two numbers using == and === and log the result to the console.
+
+let d = 65;
+let e = '65';
+
+console.log(d == e);//true
+console.log(d === e);//false
+ 
+/// === operator checks types of variable also but == only check the value is same or not
+
+
