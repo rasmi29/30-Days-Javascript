718ceb176fb6b5f3029a64b341cd65940e6fa340
Rasmiranjan Sahoo <rasmiranjansahoo702@gmail.com>
2025-02-13 11:19:16 +0530

    Day2 :- Activity 5 :- Ternary Operator

---
+++ day2/Activity5.js
@@ -0,0 +1,11 @@
+// Activity 5: Ternary Operator
+
+// Task 14: Write a program that uses the ternary operator to check if a number is positive or negative and log thi
+
+let a = 100;
+
+100 < 200 ? console.log("100 is smaller") : console.log("100 is bigger");
+
+
+
+
