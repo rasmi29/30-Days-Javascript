2e0871c616e59c39d7cfd22951f40afd9b03c8bc
Rasmiranjan Sahoo <rasmiranjansahoo702@gmail.com>
2025-02-15 01:25:06 +0530

    Day 4 --> Activity 2 --> While loop

---
+++ day4/Activity2.js
@@ -0,0 +1,26 @@
+// Activity 2: While Loop
+
+// Task 3: Write a program to calculate the sum of numbers from 1 to 10 using a while loop.
+let num = 1;
+let sum = 0;
+
+while(num<=10){
+    sum += num;
+    num++;
+}
+console.log(`sum of numbers from 1 to 10 is ${sum}.`);
+
+
+// Task 4: Write a program to print numbers from 10 to 1 using a while loop.
+
+let number = 10;
+while(number>=1){
+    console.log(number);
+    number--;
+    
+}
+
+
+
+
+
