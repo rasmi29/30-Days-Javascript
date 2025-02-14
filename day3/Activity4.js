8c317df6537f3bb5be77f97c662c9b829205dafb
Rasmiranjan Sahoo <rasmiranjansahoo702@gmail.com>
2025-02-15 00:43:27 +0530

    Day 3:- Assignment 4 :- ternary Operator

---
+++ day3/Activity4.js
@@ -0,0 +1,7 @@
+// Activity 4: Conditional (Ternary) Operator
+
+// Task 6: Write a program that uses the ternary operator to check if a number is even or add and log the result to the console.
+
+let num = 23;
+num % 2 === 0 ? console.log(`even`) : console.log(`odd`);
+
