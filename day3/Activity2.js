42c1f03543af0d3008303fa128b44367d736fb56
Rasmiranjan Sahoo <rasmiranjansahoo702@gmail.com>
2025-02-15 00:05:27 +0530

    Day2 :- ACtivity2 :- Nested If Else statements

---
+++ day3/Activity2.js
@@ -0,0 +1,29 @@
+// Activity 2: Nested If-Else Statements
+
+// Task 3: White a program to find the largest of three numbers using nested if-else statements
+
+let a = 12 ;
+let b= 34;
+let c= 45;
+
+
+if (a > b) {  
+    if (a > c) {  
+        console.log(`The largest number is ${a}`);
+        
+    } else {  
+        console.log(`The largest number is ${c}`);
+    }
+} else {  
+    if (b > c) {  
+        console.log(`The largest number is ${b}`);
+    } else {  
+        console.log(`The largest number is ${c}`);
+    }
+}
+
+
+
+
+
+
