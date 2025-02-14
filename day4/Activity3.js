33d5c0754c590bf230f3989da998038d18609305
Rasmiranjan Sahoo <rasmiranjansahoo702@gmail.com>
2025-02-15 01:33:36 +0530

    Day4  --> Activity 3 --> Do While Loop

---
+++ day4/Activity3.js
@@ -0,0 +1,39 @@
+// Activity 3: Do...While Loop
+
+// Task 5: Write a program to print numbers from 1 to 5 using a do...while loop.
+let num = 1;
+do {
+  console.log(num);
+  num++;
+} while (num <= 5);
+
+// Task 6: Write a program to calculate the factorial of a number using a do...while loop.
+
+let result = 1;
+let i = 1;
+let n = 5;
+
+do {
+  result *= i;
+  i++;
+} while (i <= n);
+console.log(`factorial of number ${n} is ${result}.`);
+
+
+// Activity 4: Nested Loops
+
+// • Task 7: Write a program to print a pattern using nested for loops:
+
+// (ignore color)
+
+// *
+
+// **
+
+// ***
+
+// Activity 5: Loop Control Statements
+
+// Task 8: Write a program to print numbers from 1 to 10, but skip the number 5 using the continue staternent.
+
+// Task 9: Write a program to print numbers from 1 to 10, but stop the loop when the number is 7 using the break sta
