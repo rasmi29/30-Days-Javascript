16024b90bd944f5fc04f35e92cc4d0fbc16190b1
Rasmiranjan Sahoo <rasmiranjansahoo702@gmail.com>
2025-02-15 01:43:40 +0530

    Day-4 --> Activity 4 --> Nested loops

--- day4/Activity3.js
+++ day4/Activity3.js
@@ -20,20 +20,3 @@ do {
 console.log(`factorial of number ${n} is ${result}.`);
 
 
-// Activity 4: Nested Loops
-
-// • Task 7: Write a program to print a pattern using nested for loops:
-
-// (ignore color)
-
-// *
-
-// **
-
-// ***
-
-// Activity 5: Loop Control Statements
-
-// Task 8: Write a program to print numbers from 1 to 10, but skip the number 5 using the continue staternent.
-
-// Task 9: Write a program to print numbers from 1 to 10, but stop the loop when the number is 7 using the break sta
