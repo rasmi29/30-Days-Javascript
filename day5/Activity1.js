3ac9ac95ae01892cdccb62e43c06975430d6dc80
Rasmiranjan Sahoo <rasmiranjansahoo702@gmail.com>
2025-02-15 10:42:52 +0530

    Day 5 --> Activity 1 --> Function Declaration

--- day5/Activity1.js
+++ day5/Activity1.js
@@ -4,7 +4,24 @@
 
 // Task 1: Write a function to check if a number is even or odd and log the result to the console.
 
+function checkNumber (num){
+    if(num % 2 === 0){
+        console.log(`even number`);
+    }else{
+        console.log(`odd number`);
+    }
+}
+
+checkNumber(3);//odd number
+checkNumber(4);// even number
+checkNumber(5);//odd number
+
+
 // Task 2: Write a function to calculate the square of a number and return the result.
 
+function calculateSquare(num){
+    return num*num;
+}
 
+console.log(calculateSquare(5));//25
 
