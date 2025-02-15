5f3e09a0df2959f42e81fa3ecf975e9251ce0e2a
Rasmiranjan Sahoo <rasmiranjansahoo702@gmail.com>
2025-02-15 11:12:06 +0530

    Day 5 --> Activity 2 -->Function Expression

--- day5/Activity2.js
+++ day5/Activity2.js
@@ -3,4 +3,21 @@
 
 // Task 3: Write a function expression to find the maximum of two numbers and log the result to the console.
 
+const maximum = function calculate(a,b){
+    if(a>b){
+        return a;
+    }else{
+        return b;
+    }
+}
+
+console.log(maximum(3,5));
+
+
 // Task 4: White a function expression to concatenate two strings and return the result.
+
+const concatenate = function add(str1, str2){
+    return str1+str2;
+}
+
+console.log(concatenate('Rasmiranjan ','Sahoo'));
