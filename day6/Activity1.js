a0eb9f0db33e983bb5fabbb60ae1cc5f3967ab5a
Rasmiranjan Sahoo <rasmiranjansahoo702@gmail.com>
2025-02-15 16:23:09 +0530

    Day 6 --> Activity 1: -->Array Creation and Access

--- day6/Activity1.js
+++ day6/Activity1.js
@@ -6,4 +6,10 @@
 
 // Task 1: Create an array of numbers from 1 to 5 and log the array to the console.
 
+const arr = [1,2,3,4,5];
+console.log(arr);//[1,2,3,4,5]
+
 // • Task 2: Access the first and last elements of the array and log them to the console.
+
+console.log(`first element of array is ${arr[0]}.`);
+console.log(`last element of array is ${arr[arr.length-1]}.`);
