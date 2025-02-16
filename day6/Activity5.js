7347b3c78a2c064b53425b13c725de22ed194826
Rasmiranjan Sahoo <rasmiranjansahoo702@gmail.com>
2025-02-16 16:40:11 +0530

    Day 6 --> Activity 5 --> Multi-dimensional Arrays

--- day6/Activity5.js
+++ day6/Activity5.js
@@ -2,4 +2,25 @@
 
 // Task 12: Create a two-dimensional array (matrix) and log the entire array to the console.
 
+let arr = [
+  [1, 2, 3],
+  [4, 5, 6],
+  [7, 8, 9],
+];
+//display in array format
+console.log(arr);
+// display in matrix format
+for (let i = 0; i < arr.length; i++) {
+  let row = "";
+  for (let j = 0; j < arr[i].length; j++) {
+      row += arr[i][j] + " ";
+  }
+  console.log(row.trim());
+}
+
 // Task 13: Access and log a specific element from the two-dimensional array.
+
+//let we have to know the 2 nd elemnt of 2nd row
+
+console.log(`2nd element from 2nd row of the array is ${arr[1][1]}`);
+
