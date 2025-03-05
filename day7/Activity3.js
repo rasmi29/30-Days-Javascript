88f8a24ba1d51db9e7bd158878386353bfa18366
Rasmiranjan Sahoo <rasmiranjansahoo702@gmail.com>
2025-03-05 22:31:42 +0530

    Day 7 --> Activity 3 --> Nested Objects

--- day7/Activity3.js
+++ day7/Activity3.js
@@ -1,3 +1,34 @@
 // Activity 3: Nested Objects
 
-// Task 5: Task 6 Create a nested object representing Access and log the name of with the library and the titles of all the boonste and books (an array of book objects), and in the library log the library object to the console.
+// Task 5: Create a nested object representing a library with properties like books(an array of book objects) and name, and log the obj
+
+const library = {
+    name: "City Central Library",
+    location: "Delhi ,India",
+    books: [
+        {
+            title: "The Great India",
+            author: "Rasmiranjan Sahoo",
+            year: 2025
+        },
+        {
+            title: "1994",
+            author: "Ramesh Sahoo",
+            year: 2020
+        },
+        {
+            title: "To Kill a worker",
+            author: "himanshu ketaki",
+            year: 2015
+        }
+    ]
+};
+
+
+
+// Task 6   Access and log the name of the library and the titles of all the  books  in the library 
+
+library.books.forEach((book)=>{
+    console.log(`Author ${book.author} written the book "${book.title}" .`);
+    
+})
