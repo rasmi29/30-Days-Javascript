71872f420426fca762035a09c22da191f08ef371
Rasmiranjan Sahoo <rasmiranjansahoo702@gmail.com>
2025-03-06 21:21:51 +0530

    Day 7 -->  This key word

--- day7/Activity4.js
+++ day7/Activity4.js
@@ -1,3 +1,34 @@
 // Activity 4: The this Keyword
 
 // • Task 7: Add a method to the book object that uses the this keyword to return a string with the book's title and year, and log the result of calling this method.
+
+
+const library = {
+    name: "City Central Library",
+    location: "Delhi ,India",
+    books: [
+        {
+            title: "The Great India",
+            author: "Rasmiranjan Sahoo",
+            year: 2025
+        },
+        {
+            title: "1994",
+            author: "Ramesh Sahoo",
+            year: 2020
+        },
+        {
+            title: "To Kill a worker",
+            author: "himanshu ketaki",
+            year: 2015
+        }
+    ]
+};
+
+library.books.forEach((book)=>{
+    book.show = function(){
+        console.log(`title : ${this.title} , year :  ${this.year}`);
+    }
+})
+
+library.books[0].show()
