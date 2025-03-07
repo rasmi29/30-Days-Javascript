9b1f4dc83d43d99547cae8d1db97a35355c84d5d
Rasmiranjan Sahoo <rasmiranjansahoo702@gmail.com>
2025-03-07 20:11:51 +0530

    Day 8 --> ES6 --> Template  literals

---
+++ day8/Activity1.js
@@ -0,0 +1,20 @@
+// Day 8: ES6+ Features
+
+// Tasks/Activities:
+
+// Activity 1: Template Literals
+
+// Task 1: Use template literals to create a string that includes variables for a person's name and age, and log the string to the console.
+
+const person = {
+    name :  "rasmiranjan",
+    age: 22
+}
+
+console.log(`person name is ${person.name} and age is ${person.age} `);
+
+
+// Task 2 Create a multi-ine string using template literals and log it to the console.
+
+console.log(`name : ${person.name}
+age : ${person.age}`);
