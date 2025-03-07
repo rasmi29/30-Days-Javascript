18ec526ec8471e0e685db83c82a3dadb83097112
Rasmiranjan Sahoo <rasmiranjansahoo702@gmail.com>
2025-03-07 19:59:09 +0530

    Day  7 --> Activity 5 --> Object iteration

--- day7/Activity5.js
+++ day7/Activity5.js
@@ -1,3 +1,18 @@
 // Activity 5: Object Iteration
 
-// Task 8: Use a fur...in loop to iterate ever the properties of the book object and log each property and its value. Task 9: Use Object.keys and Object.values methods to log all the keys and values of the book object.
+// Task 8: Use a for...in loop to iterate ever the properties of the book object and log each property and its value. 
+
+const book= {
+    name:"rasmi",
+    poet:"ranjan",
+    year:2025
+}
+
+for(key in book){
+    console.log(`${key}:${book[key]}`);
+}
+
+// Task 9: Use Object.keys and Object.values methods to log all the keys and values of the book object. 
+
+console.log(Object.keys(book));// returns a array  of keys
+console.log(Object.values(book));// returns a array of values
